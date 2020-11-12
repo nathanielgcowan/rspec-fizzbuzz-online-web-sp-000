91d276db4eb4046bfd8b36a377f6db575ac637e5
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-11-12 16:41:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz
-
+  if int % 3 == 0 && int %5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
 end
